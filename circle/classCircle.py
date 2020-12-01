6745b062c2db8e1ddc3e20b147bdede573d2b593
Vadim Pogarsky <vadim.ska8@yandex.ru>
2020-12-01 15:36:39 +0300

    added task module C 1.9

---
+++ circle/classCircle.py
@@ -0,0 +1,6 @@
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def get_area(self):
+        return self.radius ** 2 * 3.14
---
+++ circle/radius.py
@@ -0,0 +1,7 @@
+from classCircle import Circle
+
+s = Circle(int(input("Введите радиус, чтобы получить площадь круга: ")))
+
+print('Площадь круга =', s.get_area())
+
+
