6c4fc70fedb26bf17c0dfa931187909eb2f6a68c
Vadim Pogarsky <vadim.ska8@yandex.ru>
2021-03-07 15:58:41 +0300

    upload new method moldels

--- NewsPaper/NewsPaper/news/models.py
+++ NewsPaper/NewsPaper/news/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from accounts.models import User
+from django.conf import settings
 
 
 class Author(models.Model):
@@ -9,11 +9,10 @@ class Author(models.Model):
     cвязь «один к одному» с встроенной моделью пользователей User;
     рейтинг пользователя.
     """
-    author = models.OneToOneField(User, on_delete=models.CASCADE)
-    rating = models.IntegerField(default=0)
+    author_user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author_rating = models.IntegerField(default=0)
 
     def update_rating(self):
-        # TODO
         """
         Метод update_rating() модели Author, который обновляет рейтинг пользователя, переданный в аргумент этого метода.
         Он состоит из следующего:
@@ -21,8 +20,21 @@ class Author(models.Model):
         суммарный рейтинг всех комментариев автора;
         суммарный рейтинг всех комментариев к статьям автора.
         """
-        rating = self.rating
-        pass
+
+        posts_author = Post.objects.filter(post_author=self.author_user)
+        post_rating = sum([r.post_rating for r in posts_author])
+        print(post_rating)
+        comment_rating = sum([r.comment_rating for r in Comment.objects.filter(comment_author=self.author_user)])
+        print(comment_rating)
+
+        likes_author_comment_sum = sum([r.comment_rating for r in Comment.objects.filter(comment_post__in=posts_author)])
+        print(likes_author_comment_sum)
+        self.rating = post_rating + comment_rating + likes_author_comment_sum
+        print(self.rating)
+        self.save()
+
+    def __str__(self):
+        return f'{self.author_user}'
 
 
 class Category(models.Model):
@@ -31,7 +43,10 @@ class Category(models.Model):
     Имеет единственное поле: название категории.Поле должно быть уникальным(в определении поля необходимо написать
     параметр unique = True).
     """
-    name = models.CharField(max_length=60, unique=True)
+    category_name = models.CharField(max_length=60, unique=True)
+
+    def __str__(self):
+        return f'{self.category_name}'
 
 
 class Post(models.Model):
@@ -55,38 +70,41 @@ class Post(models.Model):
         (news, 'Новость'),
     )
 
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    choice_filed = models.CharField(
+    post_author = models.ForeignKey(Author, on_delete=models.CASCADE, verbose_name='Автор')
+    choice_field = models.CharField(
         max_length=2,
         choices=POST_CHOICES,
         default=news
     )
-    time_created = models.DateTimeField(auto_now_add=True)
-    post = models.ManyToManyField(Category, through='PostCategory')
-    title = models.CharField(max_length=255)
+    time_created = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')  # default=timezone.now
+    post_category = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
     text = models.TextField('Напишите сюда свой текст вашей статьи')
-    rating = models.IntegerField(default=0)
+    post_rating = models.IntegerField(default=0, verbose_name='Рейтинг')
 
     def preview(self):
         """
         Метод preview() модели Post, который возвращает начало статьи
         (предварительный просмотр) длиной 124 символа и добавляет многоточие в конце.
         """
-        text = self.text
-        pass
+        return self.text[:125] + '...'
 
     def like(self):
         """
         Метод like() в моделях Comment и Post, который увеличивает рейтинг на единицу.
         """
-        rating = self.rating
-        pass
+        self.post_rating += 1
+        self.save()
 
     def dislike(self):
         """
         Метод dislike() в моделях Comment и Post, который уменьшает рейтинг на единицу.
         """
-        rating = self.rating
+        self.post_rating -= 1
+        self.save()
+
+    def __str__(self):
+        return f'{self.post_author}'
 
 
 class PostCategory(models.Model):
@@ -95,8 +113,11 @@ class PostCategory(models.Model):
     связь «один ко многим» с моделью Post;
     связь «один ко многим» с моделью Category.
     """
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.category} - {self.post}'
 
 
 class Comment(models.Model):
@@ -110,21 +131,25 @@ class Comment(models.Model):
     дата и время создания комментария;
     рейтинг комментария.
     """
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.CharField(max_length=255)
-    time_created = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField(default=0)
-    
+    comment_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.TextField(null=False)
+    time_created = models.DateTimeField(auto_now_add=True)  # default=timezone.now
+    comment_rating = models.IntegerField(default=0)
+
     def like(self):
         """
         Метод like() в моделях Comment и Post, который увеличивает рейтинг на единицу.
         """
-        rating = self.rating
-        pass
+        self.comment_rating += 1
+        self.save()
 
     def dislike(self):
         """
         Метод dislike() в моделях Comment и Post, который уменьшает рейтинг на единицу.
         """
-        rating = self.rating
+        self.comment_rating -= 1
+        self.save()
+
+    def __str__(self):
+        return f'{self.comment_author}'
